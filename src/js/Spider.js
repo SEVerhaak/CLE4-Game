c9e019ad4379fee4d70b5ffff3f394801064b3dc
SEVerhaak <severhaak@hotmail.nl>
2024-06-12 11:44:09 +0200

    enemy inhertance gefixt

--- src/js/Spider.js
+++ src/js/Spider.js
@@ -1,8 +1,9 @@
 import { Actor, Animation, CollisionType, range, SpriteSheet, Vector } from "excalibur";
 import { Resources } from './resources.js';
 import { Player } from "./player.js";
+import {Enemy} from "./enemy.js";
 
-export class Spider extends Actor {
+export class Spider extends Enemy {
     currentAnimation = null;
     detectionRadius = 120; // Radius to detect the player
     attackradius = 70;
@@ -58,87 +59,6 @@ export class Spider extends Actor {
         // Default start animation
         this.graphics.use(this.animationRight);
         this.currentAnimation = this.animationRight;
-        this.changeDirection();
         // this.on('collisionstart', (evt) => this.onCollisionStart(evt));
     }
-
-    changeDirection() {
-        const directions = [
-            new Vector(1, 0),   // Right
-            new Vector(-1, 0),  // Left
-            new Vector(0, 1),   // Down
-            new Vector(0, -1),  // Up
-            new Vector(0, 0)    // Still
-        ];
-        const randomDirection = directions[Math.floor(Math.random() * directions.length)];
-        this.direction = randomDirection;
-        this.updateAnimation(); // Update animation based on direction
-    }
-
-    updateAnimation() {
-        if (this.direction.equals(new Vector(1, 0))) {
-            if (this.currentAnimation !== this.animationRight) {
-                this.graphics.use(this.animationRight);
-                this.currentAnimation = this.animationRight;
-            }
-        } else if (this.direction.equals(new Vector(-1, 0))) {
-            if (this.currentAnimation !== this.animationLeft) {
-                this.graphics.use(this.animationLeft);
-                this.currentAnimation = this.animationLeft;
-            }
-        }
-    }
-
-    onPreUpdate(engine, delta) {
-        let playerFound = false;
-        let playerPosition = 0; // Initialize player position
-
-        engine.currentScene.actors.forEach(actor => {
-            if (actor instanceof Player) {
-                playerFound = true;
-                const distanceToPlayer = this.pos.distance(actor.pos);
-
-                if (distanceToPlayer <= this.detectionRadius) {
-                    this.collisionType = CollisionType.Passive;
-                    this.direction = actor.pos.sub(this.pos).normalize();
-                    playerPosition = actor.pos.x - this.pos.x; // Calculate player position relative to the bat
-                    this.animationAttack.flipHorizontal = !(playerPosition > 0);
-                    if (!(playerPosition > 0)) {
-                        this.graphics.use(this.animationLeft)
-                        this.currentAnimation = this.animationLeft
-                    } else {
-                        this.graphics.use(this.animationRight)
-                        this.currentAnimation = this.animationRight
-                    }
-                    if (distanceToPlayer <= this.attackradius) {
-                        // Set animation flip based on player position relative to the bat
-                        this.graphics.use(this.animationAttack);
-                        this.currentAnimation = this.animationAttack;
-                        this.vel = this.direction.scale(this.attackSpeed);
-                    } else {
-                        this.vel = this.direction.scale(this.normalSpeed);
-                    }
-                } else {
-                    this.collisionType = CollisionType.Active;
-                    this.timeSinceLastChange += delta;
-                    if (this.timeSinceLastChange >= this.changeDirectionInterval) {
-                        this.changeDirection();
-                        this.timeSinceLastChange = 0;
-                    }
-                    this.vel = this.direction.scale(this.normalSpeed);
-                }
-            }
-
-        });
-
-        if (!playerFound) {
-            this.timeSinceLastChange += delta;
-            if (this.timeSinceLastChange >= this.changeDirectionInterval) {
-                this.changeDirection();
-                this.timeSinceLastChange = 0;
-            }
-            this.vel = this.direction.scale(this.normalSpeed);
-        }
-    }
-
 }
--- src/js/game.js
+++ src/js/game.js
@@ -44,6 +44,7 @@ export class Game extends Engine {
     startGame() {
         console.log("start de game!")
         // go to specific scene
+        this.goToLevel1();
     }
 
     goToOverWorld() {
