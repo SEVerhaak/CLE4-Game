1a6a818614fe3bc438da044043942b694cb80b43
Geert van der Velden <1079252@hr.nl>
2024-06-18 15:16:09 +0200

    add sound

--- src/js/game.js
+++ src/js/game.js
@@ -58,12 +58,12 @@ export class Game extends Engine {
 
         // go to specific scene
         //this.goToOverWorld();
-        this.goToLevel3();
+         //this.goToLevel3();
         //this.goToEndcredits();
         //this.goToEndcredits();
         this.goToOverWorld();
         //this.goToLevel3(this);
-        this.goToGameOverScene();
+        // this.goToGameOverScene();
         //this.goToStoryScene();
         // this.goToStoryScene();
     }
